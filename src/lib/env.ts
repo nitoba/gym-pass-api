823b44e4d5bde29696d3b8d2d47668069ffa8fbe
Bruno Alves <bruno@sthorm.io>
2023-03-07 22:50:47 -0300

    feat: configure database

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
 })
